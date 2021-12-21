6b5acf402e586b41ed45df7ffe1913c2fe36f8f1
JPWilson2021 <wilsonjp805@gmail.com>
2021-12-20 21:49:03 -0500

    Moved file to this repo

---
+++ src/test/java/org/wecancoeit/reviews/KdramaReviewsRepositoryTest.java
@@ -0,0 +1,31 @@
+package org.wecancoeit.reviews;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Collection;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class KdramaReviewsRepositoryTest {
+
+    private KdramaReviewsRepository underTest;
+    private Kdrama kdramaOne = new Kdrama(1L, "kdrama one title", "description");
+    private Kdrama kdramaTwo = new Kdrama(2L, "kdrama two title", "description");
+
+    @Test
+    public void shouldFindKdramaOne() {
+        underTest = new KdramaReviewsRepository(kdramaOne);
+        Kdrama foundKdrama = underTest.findOne(1L);
+        assertEquals(kdramaOne, foundKdrama);
+    }
+
+    @Test
+    public void shouldFindKdramaOneAndKdramaTwo() {
+        underTest = new KdramaReviewsRepository(kdramaOne, kdramaTwo);
+        Collection<Kdrama> foundKdramas = underTest.findAll();
+        assertThat(foundKdramas).contains(kdramaOne, kdramaTwo);
+
+
+    }
+}
